c3eb53202dd12f8c2c00d6b782bf0db440e379e6
RicardoJorge <ricardo.ricjose@gmail.com>
2017-12-10 15:06:19 +0000

    Added DataLoadingForFriendGroup

---
+++ SecretSanta.Data.Tests/FileLoaderTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SecretSanta.Data.Implementations;
+using SecretSanta.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Data.Tests
+{
+    [TestClass]
+    public class FileLoaderTest
+    {
+        private FileLoader FileLoaderMock;
+
+        [TestInitialize]
+        public void Initialzie()
+        {
+            this.FileLoaderMock = new Mock<FileLoader>().Object;
+        }
+
+        [TestMethod]
+        public void LoadDataFileForType_GetsExistingType_LoadsStream()
+        {
+            //arrage
+
+            //act
+            var result = this.FileLoaderMock.LoadDataFileForTypeAsync(typeof(FriendGroup));
+
+            //assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ SecretSanta.Data/Contracts/IFileLoader.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SecretSanta.Data.Contracts
+{
+    public interface IFileLoader
+    {
+        Task<string> LoadDataFileForTypeAsync(Type type);
+    }
+}
